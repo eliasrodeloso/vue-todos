21aafc1e7feac8adb9467c94f7b7fe7a491dbe88
Elías Rodelo <elias.rodelo@talosdigital.com>
2018-05-22 14:01:11 -0500

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,28 +15,10 @@ module.exports = {
         use: ['vue-style-loader', 'css-loader']
       },
       {
-        test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.sass$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            scss: ['vue-style-loader', 'css-loader', 'sass-loader'],
-            sass: [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax'
-            ]
-          }
+          loaders: {}
           // other vue-loader options go here
         }
       },
@@ -46,18 +28,10 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 1000,
-          name: '[name].[hash:7].[ext]'
         }
       }
     ]
