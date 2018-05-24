6924a2fc1f4f746296fb832360a10c174006a98e
Elías Rodelo <elias.rodelo@talosdigital.com>
2018-05-24 16:40:59 -0500

    assetsPublicPath changed

--- config/index.js
+++ config/index.js
@@ -48,8 +48,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-todos',
+    assetsSubDirectory: 'static/',
+    assetsPublicPath: '/vue-todos/',
 
     /**
      * Source Maps
